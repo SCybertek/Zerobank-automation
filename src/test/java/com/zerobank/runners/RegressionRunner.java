3dff1468dd1a6ddbdb2f0c254e667aab7c9de5b2
TugbaPolat <tugbapolat.usa@gmail.com>
2020-05-16 01:21:34 -0400

    added RegressionRunner

--- src/test/java/com/zerobank/runners/RegressionRunner.java
+++ src/test/java/com/zerobank/runners/RegressionRunner.java
@@ -4,22 +4,16 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-
 @RunWith(Cucumber.class)
 @CucumberOptions(
         glue = "com/zerobank/stepdefinitions" ,
         features = "src/test/resources/features" ,
         dryRun = false,
-        tags = "@smoke_test",
+        tags = "@not @smoke_test",
         plugin = {"html:target/default-report-for-regression",
                 "json:target/cucumber_regression.json" ,
                 "rerun:target/rerun_regression.txt"
         }
 )
-
-public class CukesRunner {
-
-
-
-
+public class RegressionRunner {
 }
